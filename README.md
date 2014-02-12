5a6de89091dfb4e2a988935079f3bbda3f4adda2
Sai001 <julien_lheureux3@hotmail.com>
2014-02-12 01:04:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Exercice 3
+
+Sio-2109 programmation
+
+Julien L'Heureux Houde
+
+Github : Sai001
---
+++ bin/ex03.dart
@@ -0,0 +1,17 @@
+void main() {
+  
+  var p = true;
+  var q = false;
+  var r = !q;
+  var s = false;
+
+  
+  
+  assert(p && r);
+  assert(p && !q);
+  assert(p || q);
+  assert(!(q && s));
+  assert(!(p && s));
+  
+  
+}
